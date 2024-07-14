491fea74e4e185c3d9593687c16295efc00dd4d9
Winterson <u19cse1028@cit.ac.in>
2024-07-14 20:26:56 +0530

    add test for valid json response from user register route

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,6 @@ import type { Request, Response } from "express";
 
 export class AuthController {
 	register(_req: Request, res: Response): void {
-		res.status(201).send("registration successful");
+		res.status(201).json();
 	}
 }
--- tests/user/register.spec.ts
+++ tests/user/register.spec.ts
@@ -18,6 +18,21 @@ describe("POST /auth/register", () => {
 			// Assert
 			expect(response.statusCode).toBe(201);
 		});
+
+		it("Should return valid json", async () => {
+			const userData = {
+				name: "Robot",
+				email: "robot@robo.mail",
+				password: "notARobot",
+			};
+
+			const response = await request(app)
+				.post("/auth/register")
+				.send(userData);
+			expect(response.headers["content-type"]).toEqual(
+				expect.stringContaining("json"),
+			);
+		});
 	});
 	describe("Incomplete input fields", () => {});
 });
