31aa57ac699c18685bf3fca80a96d2f763265e61
Winterson <u19cse1028@cit.ac.in>
2024-07-19 16:10:32 +0530

    refactor of auth controller

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
+import { UserService } from "../services/UserService";
 
 const router = Router();
-const authController = new AuthController();
+const userService = new UserService();
+const authController = new AuthController(userService);
 router.post("/register", (req, res) => authController.register(req, res)); // Controller.register is inside a callback because of the ambiguous binding of "this" keyword.
 
 export default router;
