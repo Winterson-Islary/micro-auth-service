287db2b0ad3d80e404cc143c4eb23bfcbbb2cb31
Winterson <u19cse1028@cit.ac.in>
2024-07-24 21:45:58 +0530

    add db initialization to server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
+import { AppDataSource } from "./configs/data-source";
 import { Config } from "./configs/envConfig";
 import logger from "./configs/logger";
 
-const startServer = () => {
+const startServer = async () => {
 	const PORT = Config.PORT;
 	try {
+		await AppDataSource.initialize();
+		logger.info("Successfully connected to database");
 		app.listen(PORT, () => {
 			logger.info(`Listening on PORT: ${PORT}`);
 		});
