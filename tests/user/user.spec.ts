4e739f9b4c30698b7b93657ee8484f9b936db057
Winterson <u19cse1028@cit.ac.in>
2024-07-29 01:36:40 +0530

    add test for logout route

--- tests/user/user.spec.ts
+++ tests/user/user.spec.ts
@@ -4,7 +4,9 @@ import request from "supertest";
 import type { DataSource } from "typeorm";
 import app from "../../src/app";
 import { AppDataSource } from "../../src/configs/data-source";
+import { RefreshToken } from "../../src/entity/RefreshToken";
 import { User } from "../../src/entity/User";
+import { TokenService } from "../../src/services/TokenService";
 import { Roles } from "../../src/types";
 
 describe("GET /auth/whoami", () => {
@@ -166,6 +168,119 @@ describe("POST /auth/refresh", () => {
 				}
 			}
 			expect(accessToken).toBeDefined();
+		});
+	});
+});
+
+describe("POST /auth/logout", () => {
+	let connection: DataSource;
+	let jwks: ReturnType<typeof createJWKSMock>;
+	beforeAll(async () => {
+		connection = await AppDataSource.initialize();
+		jwks = createJWKSMock("http://localhost:5501");
+	});
+	beforeEach(async () => {
+		jwks.start();
+		// Truncating DB
+		await connection.dropDatabase();
+		await connection.synchronize();
+	});
+	afterEach(() => {
+		jwks.stop();
+	});
+	afterAll(async () => {
+		await connection.destroy();
+	});
+	describe("on valid input", () => {
+		it("should return 200 on successful logout", async () => {
+			const userData = {
+				name: "Robot",
+				email: "robot@robo.mail",
+				password: "notARobot",
+			};
+			const userRepository = connection.getRepository(User);
+			const data = await userRepository.save({
+				...userData,
+				role: Roles.CUSTOMER,
+			});
+			const refreshRepository = connection.getRepository(RefreshToken);
+			const ThirtyDays = 1000 * 60 * 60 * 24 * 30;
+			const refreshRepositoryResponse = await refreshRepository.save({
+				user: data,
+				expiresAt: new Date(Date.now() + ThirtyDays),
+			});
+			const tokenService = new TokenService(refreshRepository);
+			const mockAccessToken = jwks.token({
+				sub: String(data.id),
+				role: data.role,
+			});
+			const mockRefreshToken = tokenService.generateRefreshToken({
+				sub: String(data.id),
+				role: data.role,
+				id: refreshRepositoryResponse.id,
+			});
+
+			const logoutResponse = await request(app)
+				.post("/auth/logout")
+				.set("Cookie", [
+					`ACCESS_TOKEN=${mockAccessToken};REFRESH_TOKEN=${mockRefreshToken};`,
+				])
+				.send();
+			expect(logoutResponse.statusCode).toBe(200);
+		});
+
+		it("should clear access and refresh token on success", async () => {
+			const userData = {
+				name: "Robot",
+				email: "robot@robo.mail",
+				password: "notARobot",
+			};
+			const userRepository = connection.getRepository(User);
+			const data = await userRepository.save({
+				...userData,
+				role: Roles.CUSTOMER,
+			});
+			const refreshRepository = connection.getRepository(RefreshToken);
+			const ThirtyDays = 1000 * 60 * 60 * 24 * 30;
+			const refreshRepositoryResponse = await refreshRepository.save({
+				user: data,
+				expiresAt: new Date(Date.now() + ThirtyDays),
+			});
+			const tokenService = new TokenService(refreshRepository);
+			const mockAccessToken = jwks.token({
+				sub: String(data.id),
+				role: data.role,
+			});
+			const mockRefreshToken = tokenService.generateRefreshToken({
+				sub: String(data.id),
+				role: data.role,
+				id: refreshRepositoryResponse.id,
+			});
+
+			const logoutResponse = await request(app)
+				.post("/auth/logout")
+				.set("Cookie", [
+					`ACCESS_TOKEN=${mockAccessToken};REFRESH_TOKEN=${mockRefreshToken};`,
+				])
+				.send();
+			interface Headers extends IncomingHttpHeaders {
+				"set-cookie": string[];
+			}
+			let accessToken: string | null = null;
+			let refreshToken: string | null = null;
+			const cookies =
+				(logoutResponse.headers as Headers)["set-cookie"] || [];
+			for (const cookie of cookies) {
+				if (cookie.startsWith("ACCESS_TOKEN=")) {
+					accessToken = cookie.split(";")[0].split("=")[1];
+				}
+				if (cookie.startsWith("REFRESH_TOKEN=")) {
+					refreshToken = cookie.split(";")[0].split("=")[1];
+				}
+			}
+
+			expect(accessToken).toBe("");
+			expect(refreshToken).toBe("");
 		});
 	});
 });
