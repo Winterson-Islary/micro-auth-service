1d3347a9cfc061265dde594d82e27884e6f41ef2
Winterson <u19cse1028@cit.ac.in>
2024-07-30 23:49:33 +0530

    add migration scripts

--- src/configs/data-source.ts
+++ src/configs/data-source.ts
@@ -14,6 +14,6 @@ export const AppDataSource = new DataSource({
 	synchronize: false, // Should only be true in development.
 	logging: false,
 	entities: [User, RefreshToken],
-	migrations: [],
+	migrations: ["src/migration/*.ts"],
 	subscribers: [],
 });
--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -1,6 +1,11 @@
 import path from "node:path";
 import { config } from "dotenv";
-config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+config({
+	path: path.join(
+		__dirname,
+		`../../.env.${process.env.NODE_ENV || "development"}`,
+	),
+});
 
 const {
 	PORT,
