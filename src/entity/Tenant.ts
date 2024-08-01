3d99a7b47b962fdf6fec39202937ca34d1d40d96
Winterson <u19cse1028@cit.ac.in>
2024-08-02 00:44:26 +0530

    add Tenant entity and migration

---
+++ src/entity/Tenant.ts
@@ -0,0 +1,22 @@
+//@ts-nocheck
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from "typeorm";
+
+@Entity({ name: "tenants" })
+export class Tenant {
+	@PrimaryGeneratedColumn()
+	id: number;
+	@Column("varchar", { length: 100 })
+	name: string;
+	@Column("varchar", { length: 255 })
+	address: string;
+	@UpdateDateColumn
+	updatedAt: number;
+	@CreateDateColumn
+	createdAt: number;
+}
