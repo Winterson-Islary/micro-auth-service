2decefabf6f8128c487143deccb465ea690641c0
Winterson <u19cse1028@cit.ac.in>
2024-08-05 23:04:59 +0530

    add admin/user route test

---
+++ tests/manager/create.spec.ts
@@ -0,0 +1,73 @@
+import createJWKSMock from "mock-jwks";
+import request from "supertest";
+import type { DataSource } from "typeorm";
+import app from "../../src/app";
+import { AppDataSource } from "../../src/configs/data-source";
+import { User } from "../../src/entity/User";
+import { Roles } from "../../src/types";
+
+describe("POST /users", () => {
+	let connection: DataSource;
+	let jwks: ReturnType<typeof createJWKSMock>;
+	let adminToken: string;
+
+	beforeAll(async () => {
+		connection = await AppDataSource.initialize();
+		jwks = createJWKSMock("http://localhost:5501");
+	});
+	beforeEach(async () => {
+		// Truncating DB
+		jwks.start();
+		await connection.dropDatabase();
+		await connection.synchronize();
+
+		adminToken = jwks.token({
+			sub: "1",
+			role: Roles.ADMIN,
+		});
+	});
+	afterEach(async () => {
+		jwks.stop();
+	});
+	afterAll(async () => {
+		await connection.destroy();
+	});
+
+	describe("On valid input fields", () => {
+		it("Should create an user record on success", async () => {
+			const userData = {
+				name: "Robot",
+				email: "robot@robo.mail",
+				password: "notARobot",
+			};
+			await request(app)
+				.post("/users")
+				.set("Cookie", [`ACCESS_TOKEN=${adminToken};`])
+				.send(userData);
+			const userRepository = connection.getRepository(User);
+			const users: User[] = await userRepository.find();
+			expect(users).toHaveLength(1);
+			expect((users[0] as User).role).toBe(Roles.MANAGER);
+		});
+		it("Should return status code 403 when non admin tries to create an user", async () => {
+			const nonAdmin = jwks.token({
+				sub: "1",
+				role: Roles.MANAGER,
+			});
+			const userData = {
+				name: "Robot",
+				email: "robot@robo.mail",
+				password: "notARobot",
+				role: Roles.MANAGER,
+			};
+			const response = await request(app)
+				.post("/users")
+				.set("Cookie", [`ACCESS_TOKEN=${nonAdmin};`])
+				.send(userData);
+			expect(response.statusCode).toBe(403);
+			const userRepository = connection.getRepository(User);
+			const users: User[] = await userRepository.find();
+			expect(users).toHaveLength(0);
+		});
+	});
+});
