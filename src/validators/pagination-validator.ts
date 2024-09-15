04373299115a6a6dadfe5f16d12427ed5f5aa921
Winterson <u19cse1028@cit.ac.in>
2024-09-16 03:21:18 +0530

    add: pagination request query validators

---
+++ src/validators/pagination-validator.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+
+const usersPaginationSchema = z.object({
+	curPage: z.number(),
+	perPage: z.number(),
+});
+
+export const ValidateUsersPaginationQuery = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const parsedCurPage = Number(req.query.curPage);
+		const parsedPerPage = Number(req.query.perPage);
+		const usersQuery: { curPage: number; perPage: number } = {
+			curPage: Number.isNaN(parsedCurPage) ? 1 : parsedCurPage,
+			perPage: Number.isNaN(parsedPerPage) ? 10 : parsedPerPage,
+		};
+		const validatedUsersQuery =
+			await usersPaginationSchema.parseAsync(usersQuery);
+		req.body.curPage = validatedUsersQuery.curPage;
+		req.body.perPage = validatedUsersQuery.perPage;
+		return next();
+	} catch (error) {
+		if (error instanceof z.ZodError) {
+			return res.status(400).json({ errors: error.issues });
+		}
+		return res
+			.status(400)
+			.json({ error: "missing or invalid input fields" });
+	}
+};
