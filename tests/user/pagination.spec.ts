b21c2bbde1108b26d6d9b8018c640fcc48b4504b
Winterson <u19cse1028@cit.ac.in>
2024-10-01 01:55:44 +0530

    add: test for pagination

---
+++ tests/user/pagination.spec.ts
@@ -0,0 +1,51 @@
+import createJWKSMock from "mock-jwks";
+import request from "supertest";
+import type { DataSource } from "typeorm";
+import app from "../../src/app";
+import { AppDataSource } from "../../src/configs/data-source";
+import { Roles } from "../../src/types";
+
+describe("GET /users", () => {
+	let connection: DataSource;
+	let jwks: ReturnType<typeof createJWKSMock>;
+	let adminToken: string;
+
+	beforeAll(async () => {
+		connection = await AppDataSource.initialize();
+		jwks = createJWKSMock("http://localhost:5501");
+	});
+	beforeEach(async () => {
+		jwks.start();
+		adminToken = jwks.token({
+			sub: "1",
+			role: Roles.ADMIN,
+		});
+		await connection.dropDatabase();
+		await connection.synchronize();
+	});
+	afterEach(async () => {
+		jwks.stop();
+	});
+	afterAll(async () => {
+		await connection.destroy();
+	});
+
+	it("On valid pagination input", async () => {
+		const userData = {
+			name: "Robot",
+			email: "robot@robo.mail",
+			password: "notARobot",
+			role: Roles.MANAGER,
+		};
+		await request(app)
+			.post("/users")
+			.set("Cookie", [`ACCESS_TOKEN=${adminToken};`])
+			.send(userData);
+		const queryString =
+			"/users?curPage=10&perPage=7&user=Robot&role=manager";
+		const response = await request(app)
+			.get(queryString)
+			.set("Cookie", [`ACCESS_TOKEN=${adminToken};`]);
+		expect(response.statusCode).toBe(200);
+	});
+});
